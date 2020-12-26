b88f9507e25964c82c7bf19c6543256fe4b1cf76
Artem Zenkovets <artyom.zenkovets@gmail.com>
2020-12-26 23:57:00 +0600

    Remove unused files. Setup project structure.

---
+++ src/components/App/App.tsx
@@ -0,0 +1,24 @@
+import "./App.scss";
+import logo from "../../assets/logo.svg";
+
+
+const App: React.FC = () => {
+  return (
+    <div className="app">
+      <div className="app__header">
+        <a href="https://www.aviasales.ru/" target="_blank" rel="noreferrer">
+          <img src={logo} alt="Aviasales logo" />
+        </a>
+      </div>
+      <div className="app__inner">
+        <div className="filter">
+
+        </div>
+        <div className="tickets-list">
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
